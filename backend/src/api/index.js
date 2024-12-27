456b1f1ba194e4a0203d8eb67ba646be45562489
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-27 20:02:52 +0700

    chore: cleaning code

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,12 +3,6 @@ const reports = require('./reports.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏',
-  });
-});
-
 router.use('/reports', reports);
 
 module.exports = router;
