8adfe62c1eaed4f13e48f56682f223330caee86e
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-27 19:24:01 +0700

    feat: create report service

--- backend/src/services/reports.service.js
+++ backend/src/services/reports.service.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line import/no-unresolved
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
 const { status } = require('http-status');
+const { v4: uuidv4 } = require('uuid');
 const db = require('../models/index.js');
 const { ApiError, ApiSuccess } = require('../utils/apiResponse.js');
 
@@ -17,6 +19,19 @@ async function getAll() {
   return new ApiSuccess(status.OK, 'GET REPORTS SUCCESS', reportsData);
 }
 
+async function create(body) {
+  try {
+    const createdReports = await db.Report.create({
+      id: uuidv4(),
+      ...body,
+    });
+    return new ApiSuccess(status.CREATED, 'CREATE BOOK SUCCESS', createdReports);
+  } catch (error) {
+    throw new ApiError(status.INTERNAL_SERVER_ERROR, error.message);
+  }
+}
+
 module.exports = {
   getAll,
+  create,
 };
