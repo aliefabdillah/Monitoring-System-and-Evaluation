778b433b81e6507e7a77a4c129240a53201c739f
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-27 17:37:03 +0700

    feat: setup database config

---
+++ backend/src/config/db.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { Sequelize } = require('sequelize');
+const config = require('./sequelize.config');
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+  },
+);
+
+module.exports = sequelize;
