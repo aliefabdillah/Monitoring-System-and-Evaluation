b9ca62f0a7225b00885e3257593bc06ed378df5b
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-27 22:20:45 +0700

    feat: create joi schema

---
+++ backend/src/utils/joiSchema.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+
+const createReport = Joi.object({
+  nama_program: Joi.string().required(),
+  jml_penerima: Joi.number().min(0).required(),
+  wilayah: Joi.string().required(),
+  tgl_penyaluran: Joi.date().required(),
+  catatan: Joi.string().allow(null).allow(''),
+  bukti: Joi.object({
+    mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/jpg', 'application/pdf').required(),
+    size: Joi.number().max(2 * 1024 * 1024).required(),
+  }),
+  status: Joi.string().required(),
+})
+
+const updateReport = Joi.object({
+  nama_program: Joi.string(),
+  jml_penerima: Joi.number().min(0),
+  wilayah: Joi.string(),
+  tgl_penyaluran: Joi.date(),
+  catatan: Joi.string().allow(null).allow(''),
+  bukti: Joi.object({
+    mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/jpg', 'application/pdf'),
+    size: Joi.number().max(2 * 1024 * 1024),
+  }),
+  status: Joi.string(),
+})
+
+module.exports = {
+  '/create': createReport,
+  '/update': updateReport,
+}
