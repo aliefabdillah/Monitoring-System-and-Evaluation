5faf2ffcfd87d8c4681a64eb33d05221996eed52
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-27 19:25:18 +0700

    feat: create report  controller

--- backend/src/controllers/reports.controller.js
+++ backend/src/controllers/reports.controller.js
@@ -9,6 +9,16 @@ async function getAllReports(req, res, next) {
   }
 }
 
+async function createReports(req, res, next) {
+  try {
+    const reportResult = await reportService.create(req.body);
+    res.status(reportResult.statusCode).send(reportResult);
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   getAllReports,
+  createReports,
 };
