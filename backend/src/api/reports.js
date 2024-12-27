df3b56f6d630ed7ad74f39a23fc60f7b56c0d3f8
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-27 22:21:55 +0700

    feat: use validation and uplaod to cloudinary

--- backend/src/api/reports.js
+++ backend/src/api/reports.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const reportController = require('../controllers/reports.controller.js');
+const { validate } = require('../middlewares.js')
+const { uploadFile } = require('../config/cloudinary.config.js')
 
 const router = Router();
 
 router.get('/', reportController.getAllReports);
 router.get('/statistic', reportController.getStatistic);
-router.post('/', reportController.createReports);
-router.patch('/:reportId', reportController.updateReports);
+router.post('/', uploadFile.single('bukti'), validate('/create'), reportController.createReports);
+router.patch('/:reportId', uploadFile.single('bukti'), validate('/update'), reportController.updateReports);
 router.delete('/:reportId', reportController.deleteReport);
 
 module.exports = router;
