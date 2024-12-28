54505147e34e7c95c0d013a6ca7e29a3f8af512c
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-28 18:45:28 +0700

    feat: validation update schema

--- frontend/app/types/ZodSchema.ts
+++ frontend/app/types/ZodSchema.ts
@@ -34,3 +34,30 @@ export const addReportSchema = z.object({
       "Only .jpg, .jpeg, .png and pdf formats are supported."
     ),
 });
+
+export const updateReportSchema = z.object({
+  nama_program: z.string().min(1, { message: "Field program cannot be empty" }),
+  jml_penerima: z
+    .number()
+    .min(0, { message: "field jumlah penerima cannot be empty" }),
+  provinsi: z.string().min(1, { message: "Field provinsi cannot be empty" }),
+  kabupaten_kota: z
+    .string()
+    .min(1, { message: "Field kabupaten/kota cannot be empty" }),
+  kecamatan: z.string().min(1, { message: "Field kecamatan cannot be empty" }),
+  tgl_penyaluran: z
+    .string()
+    .min(1, { message: "Field tanggal penyaluran cannot be empty" }),
+  catatan: z.string().optional(),
+  bukti: z
+    .any()
+    .optional()
+    .refine(
+      (file) => !file || (file && file.size <= MAX_FILE_SIZE),
+      `Max image size is 2MB.`
+    )
+    .refine(
+      (file) => !file || (file && ACCEPTED_FILE_TYPES.includes(file.type)),
+      "Only .jpg, .jpeg, .png and pdf formats are supported."
+    ),
+});
