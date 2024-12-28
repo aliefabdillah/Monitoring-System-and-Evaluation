aa80ccd34aefb0a97193060b9cd2f43632116754
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-28 14:41:03 +0700

    feat: report service api

---
+++ frontend/app/data/services/ReportsService.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosInstance } from "axios";
+
+export class ReportsService {
+  protected readonly instance: AxiosInstance;
+  public constructor(url: string) {
+    this.instance = axios.create({
+      baseURL: url,
+      timeout: 30000,
+      timeoutErrorMessage: "URL Time Out!",
+    });
+  }
+
+  getListReport = () => {
+    return this.instance
+      .get(`/`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch(function (error) {
+        if(error.response) {
+          const errorResponse = {
+            code: error.response.status,
+            message: error.response.statusText,
+          }
+          return errorResponse
+        } else {
+          const errorResponse = {
+            code: error.code,
+            message: error.message,
+            name: error.name
+          }
+          return errorResponse;
+        }
+      });
+  };
+}
