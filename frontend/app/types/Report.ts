46b5481d929ab6fe9839f96847f3e4540678edb5
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-28 14:40:52 +0700

    feat: model for api

---
+++ frontend/app/types/Report.ts
@@ -0,0 +1,9 @@
+export type ListReport = {
+  id: string,
+  nama_program: string,
+  jml_penerima: number,
+  provinsi: string,
+  kabupaten_kota: string,
+  kecamatan: string,
+  status: string,
+}
