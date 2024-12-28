8661d6d9ea8d1967dda62a42127a1a0dbe537198
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-28 17:11:02 +0700

    feat: integration with api create

--- frontend/app/components/Form.tsx
+++ frontend/app/components/Form.tsx
@@ -1,7 +1,13 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useActionState, useEffect, useState } from "react";
+import { addReport } from "../data/actions/formReport";
+import ZodErrors from "./ZodErrors";
+import ErrorToast from "./ErrorToast";
+import { ApiError } from "../types/ApiError";
+import SuccessModal from "./SuccessModal";
+import { useRouter } from "next/navigation";
 
 interface Province {
   id: string;
@@ -19,6 +25,30 @@ interface District {
 }
 
 export default function Form() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isToastOpen, setIsToastOpen] = useState(false);
+  const [errorData, setErrorData] = useState<ApiError>({
+    code: 0,
+    message: "",
+  });
+
+  const [formAddState, formAddAction] = useActionState(addReport, {
+    data: null,
+  });
+  const [file, setFile] = useState<File>();
+  const [formReportState, setFormReportState] = useState({
+    id: "",
+    nama_program: "",
+    jml_penerima: 0,
+    provinsi: "",
+    kabupaten_kota: "",
+    kecamatan: "",
+    tgl_penyaluran: "",
+    catatan: "",
+    bukti: "",
+  });
+
   const [provinces, setProvinces] = useState<Province[]>([]);
   const [regencies, setRegencies] = useState<Regency[]>([]);
   const [districts, setDistricts] = useState<District[]>([]);
@@ -71,12 +101,68 @@ export default function Form() {
     }
   }, [selectedRegency]);
 
+  useEffect(() => {
+    if (!formAddState.isLoading) {
+      setIsLoading(false);
+    }
+
+    if (formAddState.isError) {
+      setErrorData({
+        code: formAddState?.apiErrors?.code,
+        message: formAddState?.apiErrors?.message,
+      });
+      setIsToastOpen(true);
+    }
+
+    if (formAddState.isSuccess) {
+      (
+        document.getElementById("success_modal") as HTMLDialogElement
+      ).showModal();
+      setTimeout(() => {
+        router.push("/");
+      }, 2000);
+    }
+  }, [formAddState]);
+
+  const handleInputChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setFormReportState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files?.[0]);
+    }
+  };
+
+  const handleCloseToast = () => {
+    setIsToastOpen(false);
+  };
+
   return (
-    <form className="outline-1 outline outline-zinc-400 h-full w-2/3 py-4 px-7 rounded-md">
-      <h1 className="text-center font-poppins font-semibold text-xl my-4">
-        Report Form
-      </h1>
-      {/* {bookData && (
+    <>
+      <SuccessModal message={formAddState?.message} />
+      <ErrorToast
+        error={errorData}
+        isOpen={isToastOpen}
+        onClose={handleCloseToast}
+      />
+      <form
+        className="outline-1 outline outline-zinc-400 h-full w-2/3 py-4 px-7 rounded-md"
+        action={formAddAction}
+        onSubmit={() => setIsLoading(true)}
+      >
+        <h1 className="text-center font-poppins font-semibold text-xl my-4">
+          Report Form
+        </h1>
+        {/* {bookData && (
           <input
             type="text"
             placeholder="Type here"
@@ -87,184 +173,201 @@ export default function Form() {
             hidden
           />
         )} */}
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Nama Program <span className="text-red-700">*</span>
-          </span>
-        </div>
-        {/* <input
-            type="text"
-            id="nama_program"
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Nama Program <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <select
+            className="select select-bordered join-item"
+            // defaultValue={""}
+            value={formReportState.nama_program}
+            onChange={handleInputChange}
             name="nama_program"
-            placeholder="Type here..."
-            // value={formBookState.title}
-            // onChange={handleInputChange}
+          >
+            <option value={""} hidden>
+              Pilih program
+            </option>
+            <option value="BPNT">Bantuan Pangan Non Tunai (BPNT)</option>
+            <option value="PIP">Program Indonesia Pintar (PIP)</option>
+            <option value="JKN_KIS">
+              Jaminan Kesehatan Nasional - Kartu Indonesia Sehat (JKN-KIS)
+            </option>
+            <option value="BST">Bantuan Sosial Tunai (BST)</option>
+            <option value="Subsidi Listrik">Bantuan Subsidi Listrik</option>
+          </select>
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.nama_program} />
+          </div>
+        </label>
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Provinsi <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <select
+            value={selectedProvince}
+            onChange={(e) => setSelectedProvince(e.target.value)}
+            className="select select-bordered join-item"
+            name="provinsi"
+          >
+            <option value="">Pilih Provinsi</option>
+            {provinces.map((province) => (
+              <option key={province.id} value={province.id}>
+                {province.name}
+              </option>
+            ))}
+          </select>
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.provinsi} />
+          </div>
+        </label>
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Kabupaten/Kota <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <select
+            value={selectedRegency}
+            onChange={(e) => setSelectedRegency(e.target.value)}
+            disabled={!selectedProvince}
+            name="kabupaten_kota"
+            className="select select-bordered join-item"
+          >
+            <option value="">Pilih Kabupaten/Kota</option>
+            {regencies.map((regency) => (
+              <option key={regency.id} value={regency.id}>
+                {regency.name}
+              </option>
+            ))}
+          </select>
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.kabupaten_kota} />
+          </div>
+        </label>
+        <label className={`form-control w-full max-w-full`}>
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Kecamatan <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <select
+            value={selectedDistrict}
+            onChange={(e) => setSelectedDistrict(e.target.value)}
+            disabled={!selectedRegency}
+            name="kecamatan"
+            className="select select-bordered join-item"
+          >
+            <option value="">Pilih Kecamatan</option>
+            {districts.map((district) => (
+              <option key={district.id} value={district.id}>
+                {district.name}
+              </option>
+            ))}
+          </select>
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.kecamatan} />
+          </div>
+        </label>
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Jumlah Penerima <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <input
+            id="jml_penerima"
+            name="jml_penerima"
+            type="number"
+            inputMode="numeric"
+            value={formReportState.jml_penerima}
+            onChange={handleInputChange}
+            placeholder="2000"
             className="input bg-white input-bordered w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          /> */}
-        <select className="select select-bordered join-item" defaultValue={""}>
-          <option value="" hidden>
-            Pilih program
-          </option>
-          <option>Sci-fi</option>
-          <option>Drama</option>
-          <option>Action</option>
-        </select>
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.title || formEditState?.zodErrors?.title} /> */}
+          />
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.jml_penerima} />
+          </div>
+        </label>
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Tanggal Penyaluran <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <input
+            id="tgl_penyaluran"
+            name="tgl_penyaluran"
+            type="date"
+            value={formReportState.tgl_penyaluran}
+            onChange={handleInputChange}
+            placeholder="date"
+            className="input bg-white input-bordered w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.tgl_penyaluran} />
+          </div>
+        </label>
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">
+              Bukti <span className="text-red-700">*</span>
+            </span>
+          </div>
+          <input
+            id="bukti"
+            name="bukti"
+            type="file"
+            onChange={handleFileSelected}
+            placeholder="bukti dalam bentuk pdf"
+            className="file-input bg-white file-input-bordered w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <div className="label">
+            {file && (
+              <Link
+                href={`${URL.createObjectURL(file) || formReportState.bukti}`}
+                target="_blank"
+                download={file.name || formReportState.bukti}
+              >
+                file selected: <b>{file.name || formReportState.bukti}</b>
+              </Link>
+            )}
+            <ZodErrors error={formAddState?.zodErrors?.bukti} />
+          </div>
+        </label>
+        <label className="form-control w-full max-w-full">
+          <div className="label">
+            <span className="label-text text-zinc-900">Catatan</span>
+          </div>
+          <textarea
+            id="catatan"
+            name="catatan"
+            value={formReportState.catatan}
+            onChange={handleInputChange}
+            placeholder="Masukan catatan tambahan"
+            className="textarea textarea-bordered bg-white h-40 w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <div className="label">
+            <ZodErrors error={formAddState?.zodErrors?.catatan} />
+          </div>
+        </label>
+        <div className="flex flex-row gap-2 justify-between mt-5">
+          <Link href={"/"} className="btn btn-outline btn-warning text-white">
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className={`btn btn-info text-white ${
+              isLoading ? "btn-disabled" : ""
+            }`}
+          >
+            {isLoading ? "Loading..." : "Save"}
+          </button>
         </div>
-      </label>
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Provinsi <span className="text-red-700">*</span>
-          </span>
-        </div>
-        <select
-          value={selectedProvince}
-          onChange={(e) => setSelectedProvince(e.target.value)}
-          className="select select-bordered join-item"
-        >
-          <option value="">Pilih Provinsi</option>
-          {provinces.map((province) => (
-            <option key={province.id} value={province.id}>
-              {province.name}
-            </option>
-          ))}
-        </select>
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.author || formEditState?.zodErrors?.author} /> */}
-        </div>
-      </label>
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Kabupaten/Kota <span className="text-red-700">*</span>
-          </span>
-        </div>
-        <select
-          value={selectedRegency}
-          onChange={(e) => setSelectedRegency(e.target.value)}
-          disabled={!selectedProvince}
-          className="select select-bordered join-item"
-        >
-          <option value="">Pilih Kabupaten/Kota</option>
-          {regencies.map((regency) => (
-            <option key={regency.id} value={regency.id}>
-              {regency.name}
-            </option>
-          ))}
-        </select>
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.author || formEditState?.zodErrors?.author} /> */}
-        </div>
-      </label>
-      <label className={`form-control w-full max-w-full`}>
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Kecamatan <span className="text-red-700">*</span>
-          </span>
-        </div>
-        <select
-          value={selectedDistrict}
-          onChange={(e) => setSelectedDistrict(e.target.value)}
-          disabled={!selectedRegency}
-          className="select select-bordered join-item"
-        >
-          <option value="">Pilih Kecamatan</option>
-          {districts.map((district) => (
-            <option key={district.id} value={district.id}>
-              {district.name}
-            </option>
-          ))}
-        </select>
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.author || formEditState?.zodErrors?.author} /> */}
-        </div>
-      </label>
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Jumlah Penerima <span className="text-red-700">*</span>
-          </span>
-        </div>
-        <input
-          min={1}
-          id="jml_penerima"
-          name="jml_penerima"
-          type="number"
-          // value={formBookState.year}
-          // onChange={handleInputChange}
-          placeholder="2000"
-          className="input bg-white input-bordered w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.year || formEditState?.zodErrors?.year} /> */}
-        </div>
-      </label>
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Tanggal Penyaluran <span className="text-red-700">*</span>
-          </span>
-        </div>
-        <input
-          min={1}
-          id="tgl_penyaluran"
-          name="tgl_penyaluran"
-          type="date"
-          // value={formBookState.year}
-          // onChange={handleInputChange}
-          placeholder="date"
-          className="input bg-white input-bordered w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.year || formEditState?.zodErrors?.year} /> */}
-        </div>
-      </label>
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">
-            Bukti <span className="text-red-700">*</span>
-          </span>
-        </div>
-        <input
-          id="bukti"
-          name="bukti"
-          type="file"
-          // value={formBookState.year}
-          // onChange={handleInputChange}
-          placeholder="date"
-          className="file-input bg-white file-input-bordered w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.year || formEditState?.zodErrors?.year} /> */}
-        </div>
-      </label>
-      <label className="form-control w-full max-w-full">
-        <div className="label">
-          <span className="label-text text-zinc-900">Catatan</span>
-        </div>
-        <textarea
-          id="catatan"
-          name="catatan"
-          // value={formBookState.year}
-          // onChange={handleInputChange}
-          placeholder="Masukan catatan tambahan"
-          className="textarea textarea-bordered bg-white h-40 w-full max-w-full text-zinc-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <div className="label">
-          {/* <ZodErrors error={formAddState?.zodErrors?.year || formEditState?.zodErrors?.year} /> */}
-        </div>
-      </label>
-      <div className="flex flex-row gap-2 justify-between mt-5">
-        <Link href={"/"} className="btn btn-outline btn-warning text-white">
-          Cancel
-        </Link>
-        <button type="submit" className={`btn btn-info text-white`}>
-          SAVE
-        </button>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
