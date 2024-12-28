e959bd81b6b4c8b07e543ae413367727cadc8fb4
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-28 13:49:45 +0700

    feat: refactor wilayah

--- backend/src/utils/joiSchema.js
+++ backend/src/utils/joiSchema.js
@@ -3,9 +3,12 @@ const Joi = require('joi');
 const createReport = Joi.object({
   nama_program: Joi.string().required(),
   jml_penerima: Joi.number().min(0).required(),
-  wilayah: Joi.string().required(),
+  provinsi: Joi.string().required(),
+  kabupaten_kota: Joi.string().required(),
+  kecamatan: Joi.string().required(),
   tgl_penyaluran: Joi.date().required(),
   catatan: Joi.string().allow(null).allow(''),
+  alasan: Joi.string().allow(null).allow(''),
   bukti: Joi.object({
     mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/jpg', 'application/pdf').required(),
     size: Joi.number().max(2 * 1024 * 1024).required(),
@@ -16,9 +19,12 @@ const createReport = Joi.object({
 const updateReport = Joi.object({
   nama_program: Joi.string(),
   jml_penerima: Joi.number().min(0),
-  wilayah: Joi.string(),
+  provinsi: Joi.string(),
+  kabupaten_kota: Joi.string(),
+  kecamatan: Joi.string(),
   tgl_penyaluran: Joi.date(),
   catatan: Joi.string().allow(null).allow(''),
+  alasan: Joi.string().allow(null).allow(''),
   bukti: Joi.object({
     mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/jpg', 'application/pdf'),
     size: Joi.number().max(2 * 1024 * 1024),
