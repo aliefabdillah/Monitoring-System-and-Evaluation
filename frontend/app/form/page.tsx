813f363c37f30762e439c19bd37983dfac612c24
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-29 14:57:04 +0700

    refactor: suspense to parent component

--- frontend/app/form/page.tsx
+++ frontend/app/form/page.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Base from "../components/Base";
 import Form from "../components/Form";
 
 export default function FormPage() {
   return (
-    <>
+    <Suspense>
       <Base>
         <Form />
       </Base>
-    </>
+    </Suspense>
   );
 }
