1eb5669c27b5a9ee19e3bcea109479b73f9bff74
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-29 12:25:35 +0700

    feat: add chart

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,10 +1,12 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import BasePage from "./components/BasePage";
 import { Statistic } from "./types/Report";
 import { ApiError } from "./types/ApiError";
 import { reportsService } from "./data/services";
 import ErrorToast from "./components/ErrorToast";
+import RecipientProgramChart from "./components/RecipientProgramChart";
+import DistributionChart from "./components/DistributionChart";
 
 export default function Home() {
   const [isToastOpen, setIsToastOpen] = useState(false);
@@ -46,8 +48,16 @@ export default function Home() {
       <div className="w-2/3">
         {/* <StatisticCard /> */}
         <p className="font-poppins font-normal text-3xl">
-          Total Laporan - <span className="font-bold">{statisticData?.totalReports}</span>
+          Total Laporan -{" "}
+          <span className="font-bold">{statisticData?.totalReports}</span>
         </p>
+        <div className="divider"></div>
+        <RecipientProgramChart
+          recipientData={statisticData ? statisticData.recipientsPerProgram : []}
+        />
+        <div className="divider"></div>
+        <DistributionChart distributionData={statisticData ? statisticData.distributionByProvince : []}/>
+        
       </div>
     </BasePage>
   );
