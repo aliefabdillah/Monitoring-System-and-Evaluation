025435f03b69c84b6a2dbc35839518aedd1f25a3
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-29 13:49:42 +0700

    fix: config vercel

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,6 +3,12 @@ const reports = require('./reports.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - Monitoring and Management System',
+  });
+});
+
 router.use('/reports', reports);
 
 module.exports = router;
